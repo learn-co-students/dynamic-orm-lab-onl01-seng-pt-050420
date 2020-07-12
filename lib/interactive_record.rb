eaa12c55d0300bc4272b9f54852323a2b8f52638
cedric-joaquin <joaquin.cedric@gmail.com>
2020-07-12 16:22:05 -0700

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,16 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+    def self.table_name
+       self.to_s.downcase.pluralize 
+    end
+
+    def self.column_names
+        columns = []
+        DB[:conn].execute("PRAGMA table_info(#{table_name})").each do |col|
+            columns << col["name"] unless col["name"] == nil
+        end
+        columns
+    end
 end
