16e87df5a0d674328da585e8db0f93574b42e42e
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-12 22:11:00 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,13 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def initialize(options = {})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
