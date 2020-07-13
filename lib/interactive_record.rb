e5299e0e894decf780f0fc832e79ae743557f3de
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-12 22:15:48 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -24,4 +24,20 @@ class InteractiveRecord
     end
     column_names.compact
   end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col.name == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
 end
