24945451ffb6053b46f7841bb4cf77bf33cf6c7c
kanu-e <chukwuemekakanu@gmail.com>
2020-07-26 17:50:42 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
-
+   
+  self.column_names.each {|col_name| attr_accessor col_name.to_sym}
+# binding.pry
 end
+
